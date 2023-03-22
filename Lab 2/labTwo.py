bff89331a7c82dbad2156a06ce09827693a13ea8
Tuxuy3Dom <ihor.yaremchuk2120@gmail.com>
2023-03-22 22:35:11 +0100

    Upgrate all

--- Lab 2/labTwo.py
+++ Lab 2/labTwo.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Import the pandas library
 import pandas as pd
 # Import the folium library
@@ -60,43 +59,3 @@ for lt, ln, el, nm in zip(lat, lot, elev, name):
 # Save google open street map in format .html
 map.save("labTwo.html")
 map
-=======
-# Import the pandas library
-import pandas as pd
-# Import the folium library
-import folium
- 
-# Przydatna informacja
-# https://www.python-graph-gallery.com/312-add-markers-on-folium-map
-
-data = pd.read_csv('Volcanoes.txt')
-
-map = folium.Map(location = [50.05, 21.95],tiles="OpenStreetMap", zoom_start = 4)
-
-lat = list(data["LAT"])
-lot = list(data["LON"])
-elev = list(data["ELEV"])
-name = list(data["NAME"])
-
-# Function change color in points for heigth volcans
-def set_color(elev):
-    if elev < 2000:
-        return "green"
-    elif elev < 3000:
-        return "orange"
-    else: 
-        return "red"
-
-for lt, ln, el, nm in zip(lat, lot, elev, name):
-    html=f"""
-        <h2> Imię wulkanu: "{nm}"<h2>
-        <p> Informacja dodatkowa: {el} m. </p>
-        """
-    iframe = folium.IFrame(html=html, width=200, height=200)
-    popup = folium.Popup(iframe, max_width=2650)
-    map.add_child(folium.Marker(location = [lt, ln], tooltip = nm, popup = popup, 
-                                icon = folium.Icon(color = set_color(el))))
-
-map.save("labTwo.html")
-map
->>>>>>> 870dee5d82e6303f5e88f7541d4cf96757008964
